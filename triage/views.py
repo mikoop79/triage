658ef84f62b3bd3f3375b5f4454e5e076e21e3ea
Luke Cawood <luke.cawood@99designs.com>
2011-10-14 15:25:01 +1100

    changed collection name

--- triage/views.py
+++ triage/views.py
@@ -3,12 +3,12 @@ from pymongo.objectid import ObjectId
 
 @view_config(route_name='error_list', renderer='triage:templates/error_list.html')
 def error_list(request):
-	errors = request.db['errors'].find()
+	errors = request.db['contest-error'].find()
 	return {'errors': errors}
 
 
 @view_config(route_name='error_view', renderer='triage:templates/error_view.html')
 def error_view(request):
 	error_id = request.matchdict['id']
-	error = request.db['errors'].find_one({'_id':ObjectId(error_id)})
+	error = request.db['contest-error'].find_one({'_id':ObjectId(error_id)})
 	return { 'error' : error }
