838bf67e79e94c197b07d8c43de519a33a80ac25
Luke Cawood <luke.cawood@99designs.com>
2011-10-14 16:48:02 +1100

    First shot at aggregation :)

--- triage/views.py
+++ triage/views.py
@@ -1,14 +1,16 @@
 from pyramid.view import view_config
 from pymongo.objectid import ObjectId
+from triage.helpers import get_errors
 
 @view_config(route_name='error_list', renderer='triage:templates/error_list.html')
 def error_list(request):
-	errors = request.db['contest-error'].find()
+
+	errors = get_errors(request, 'contest-errors')
 	return {'errors': errors}
 
 
 @view_config(route_name='error_view', renderer='triage:templates/error_view.html')
 def error_view(request):
 	error_id = request.matchdict['id']
-	error = request.db['contest-error'].find_one({'_id':ObjectId(error_id)})
+	error = request.db['contest-errors'].find_one({'_id':ObjectId(error_id)})
 	return { 'error' : error }
