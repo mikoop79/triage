545e38e86bbe952e9ecbf53d3ec9d0cd20d47d85
Luke Cawood <luke.cawood@99designs.com>
2011-10-14 14:05:18 +1100

    Integrated live data with design

--- triage/views.py
+++ triage/views.py
@@ -3,13 +3,5 @@ from pyramid.view import view_config
 
 @view_config(route_name='index', renderer='triage:templates/main.html')
 def my_view(request):
-    return {'project': 'triage'}
-
-@view_config(route_name='jinjatest', renderer='jinjatest.jinja2')
-def jinjatest(request):
-	return {'hello': 'world'}
-
-@view_config(route_name='mongo', renderer='mongo.jinja2')
-def mongo(request):
 	errors = request.db['errors'].find()
 	return {'errors': errors}
