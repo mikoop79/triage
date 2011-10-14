4f908e877720d5fb8d19d06d7a50c45d9b06f72b
Luke Cawood <luke.cawood@99designs.com>
2011-10-14 13:41:58 +1100

    loading errors from mongo

--- triage/routes.py
+++ triage/routes.py
@@ -2,4 +2,4 @@
 def configure_routes(config):
 	config.add_route('index', '/')
 	config.add_route('jinjatest', '/jinja/{test}')
-
+	config.add_route('mongo', 'mongo')
--- triage/views.py
+++ triage/views.py
@@ -1,9 +1,14 @@
 from pyramid.view import view_config
 
-@view_config(route_name='index', context='triage:resources.Root',  renderer='triage:templates/mytemplate.pt')
+@view_config(route_name='index', renderer='triage:templates/mytemplate.pt')
 def my_view(request):
     return {'project':'triage'}
 
-@view_config(route_name='jinjatest', context='triage:resources.Root', renderer='jinjatest.jinja2')
+@view_config(route_name='jinjatest', renderer='jinjatest.jinja2')
 def jinjatest(request):
 	return {'hello':'world'}
+
+@view_config(route_name='mongo', renderer='mongo.jinja2')
+def mongo(request):
+	errors = request.db['errors'].find()
+	return {'errors':errors}
