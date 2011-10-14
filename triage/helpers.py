18386928524c99d9ed6c025fb46bdea91e8a8b49
Luke Cawood <luke.cawood@99designs.com>
2011-10-14 17:28:09 +1100

    fixed whitespace

--- triage/helpers.py
+++ triage/helpers.py
@@ -6,35 +6,35 @@ def get_errors(request, collection):
 	map = Code("""
 
 	function() {
-	  emit( this.type +":"+ this.line +":"+ this.file, {
-	    message: this.message,
-	    type: this.type,
-	    line: this.line,
-	    file: this.file,
-	    oldest: this.timestamp,
-	    youngest: this.timestamp,
-	    _id: this._id,
-	    count:1 
-	  });
+		emit( this.type +":"+ this.line +":"+ this.file, {
+			message: this.message,
+			type: this.type,
+			line: this.line,
+			file: this.file,
+			oldest: this.timestamp,
+			youngest: this.timestamp,
+			_id: this._id,
+			count:1 
+		});
 	}""")
 
 	reduce = Code("""
 
 	function(key, values) {
-	  var result = values.pop();
+		var result = values.pop();
 
-	  values.forEach(function(value) {
-	    if (value.oldest < result.oldest) {
-	      result.oldest = value.oldest;
-	      result._id = value._id;
-	    }
-	    if (value.youngest > result.youngest) {
-	      result.message = value.message;
-	      result.youngest = value.youngest;
-	    }
+		values.forEach(function(value) {
+			if (value.oldest < result.oldest) {
+				result.oldest = value.oldest;
+			}
+			if (value.youngest > result.youngest) {
+				result.message = value.message;
+				result.youngest = value.youngest;
+				result._id = value._id;
+			}
 
-	    result.count += value.count;
-	  });
-	  return result;
+			result.count += value.count;
+		});
+		return result;
 	}""")
 	return request.db[collection].map_reduce(map, reduce, collection+'aggregate').find(fields={ 'value': 1 })
