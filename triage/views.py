d828e93f3453f49dadf3750462ed9fe5bf9d6b36
Luke Cawood <luke.cawood@99designs.com>
2011-10-14 14:24:53 +1100

    Added error view route

--- triage/views.py
+++ triage/views.py
@@ -5,3 +5,10 @@ from pyramid.view import view_config
 def error_list(request):
 	errors = request.db['errors'].find()
 	return {'errors': errors}
+
+
+@view_config(route_name='error_view', renderer='triage:templates/error_view.html')
+def error_view(request):
+	error_id = request.matchdict['id']
+	error = request.db['errors'].find_one({'_id':error_id})
+	return { 'error' : error }
