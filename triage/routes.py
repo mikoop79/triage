9978ceddc7e14626d83293ee699a34119178c163
creativej <jamisonlu@hotmail.com>
2011-11-04 14:04:24 +1100

    Added api_log route

--- triage/routes.py
+++ triage/routes.py
@@ -3,3 +3,4 @@ def configure_routes(config):
 	config.add_route('index', '/')	
 	config.add_route('error_list', '/projects/{project}')	
 	config.add_route('error_view', 'error/{id}')
+	config.add_route('api_log', 'error/{id}')
