1cf7a1d532b4366f9827d4c682e52694ec8828c3
Luke Cawood <luke.cawood@99designs.com>
2011-11-07 10:58:17 +1100

    Removed uneeded code

--- triage/resources.py
+++
@@ -1,4 +0,0 @@
-class Root(object):
-    def __init__(self, request):
-        self.request = request
-        self.projects = request.registry.settings['projects']
--- triage/views.py
+++ triage/views.py
@@ -10,16 +10,6 @@ import time
 import logging
 log = logging.getLogger(__name__)
 
-import pyramid.threadlocal as threadlocal
-from pyramid.events import BeforeRender, subscriber
-@subscriber(BeforeRender)
-def add_route_url(event):
-    request = event.get('request') or threadlocal.get_current_request()
-    if not request:
-        return
-    event['route_url'] = request.route_url
-
-
 
 @view_config(route_name='index')
 def index(request):
