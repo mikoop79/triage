c207c6d80e39a78d323d86454b8d7f885ecd0b6f
Luke Cawood <luke.cawood@99designs.com>
2011-11-07 10:46:08 +1100

    Moved mongo code into subscribers

--- triage/__init__.py
+++ triage/__init__.py
@@ -1,6 +1,4 @@
 from pyramid.config import Configurator
-from pyramid.events import subscriber
-from pyramid.events import NewRequest
 import pymongo
 
 from triage.resources import Root
@@ -22,18 +20,11 @@ def main(global_config, **settings):
     #routes
     configure_routes(config)
     # MongoDB
-    def add_mongo_db(event):
-        settings = event.request.registry.settings
-        url = settings['mongodb.url']
-        db_name = settings['mongodb.db_name']
-        db = settings['mongodb_conn'][db_name]
-        event.request.db = db
     db_uri = settings['mongodb.url']
     conn = pymongo.Connection(db_uri)
     config.registry.settings['mongodb_conn'] = conn
     config.registry.settings['projects'] = app_settings.PROJECTS
     config.registry.settings['default_project'] = app_settings.DEFAULT_PROJECT
-    config.add_subscriber(add_mongo_db, NewRequest)
     return config.make_wsgi_app()
 
 
