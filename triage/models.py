b88d8f57206f25c1e2a71bd06dd5cb7c3113e931
Josh Benham <josh.benham@99designs.com>
2011-12-16 11:48:58 +1100

    Cleaned up the code and created a User.get_user() method

--- triage/models.py
+++ triage/models.py
@@ -1,4 +1,6 @@
 import settings
+from pyramid.security import authenticated_userid
+from pymongo.objectid import ObjectId
 
 
 class Model(object):
@@ -8,12 +10,14 @@ class Model(object):
 
     def __get__(self, obj, cls=None):
         return self.data[obj]
-    
+
     def __set__(self, obj, val):
-        return (self.data[obj] = val)
-    
+        self.data[obj] = val
+        return self.data[obj]
+
     def __delete__(self, obj):
-        return (del self.data[obj])
+        del self.data[obj]
+        return self.data[obj]
 
     def save(self, collection):
         collection.save(self.data)
@@ -23,17 +27,16 @@ class Error(Model):
     pass
 
 
-
 class ErrorInstance(Model):
- 
+
     def project(self):
         return Project(self.data['application'])
 
-    def get_aggregate_identity():
+    def get_aggregate_identity(self):
         return {
             'type': self.type,
             'line': self.line,
-            'file': self.file,            
+            'file': self.file,
         }
 
 
@@ -41,7 +44,7 @@ class Project:
     @classmethod
     def from_error(cls, error):
         return cls(error['application'])
-    
+
     def __init__(self, name):
         self.settings = settings.PROJECTS[name]
 
@@ -49,6 +52,12 @@ class Project:
         return db[self.settings['collection']]
 
 
+class User(Model):
 
-class User(model):
-    pass
+    @classmethod
+    def get_user(self, request):
+        userid = authenticated_userid(request)
+        if not userid:
+            return
+
+        return request.db['users'].find_one({'_id': ObjectId(userid)})
--- triage/subscribers.py
+++ triage/subscribers.py
@@ -1,15 +1,12 @@
 import pyramid.threadlocal as threadlocal
 from pyramid.events import BeforeRender, subscriber, NewRequest
-from pyramid.security import authenticated_userid
-from pymongo.objectid import ObjectId
+from triage.models import User
 
 
 # Adds route_url method to the top level of the template context
 @subscriber(BeforeRender)
 def add_route_url(event):
     request = event.get('request') or threadlocal.get_current_request()
-    if not request:
-        return
     event['route_url'] = request.route_url
 
 
@@ -17,15 +14,7 @@ def add_route_url(event):
 @subscriber(BeforeRender)
 def add_get_user(event):
     request = event.get('request') or threadlocal.get_current_request()
-
-    if not request:
-        return
-
-    userid = authenticated_userid(request)
-    if not userid:
-        return
-
-    event['get_user'] = request.db['users'].find_one({'_id': ObjectId(userid)})
+    event['get_user'] = User.get_user(request)
 
 
 # Adds mongo db to each request
