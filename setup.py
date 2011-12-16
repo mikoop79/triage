3e0d681cb89b3a2eb16bb52479c27c10cecc4c4f
jamison.lu <jamisonlu@hotmail.com>
2011-12-16 16:07:09 +1100

    Added mongoengine to our dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.md')).read()
 
-requires = ['pyramid', 'WebError', 'pymongo', 'deform', 'pyzmq', 'msgpack-python', 'pyramid_beaker']
+requires = ['pyramid', 'WebError', 'pymongo', 'deform', 'pyzmq', 'msgpack-python', 'pyramid_beaker', 'mongoengine']
 
 setup(name='triage',
       version='0.0',
